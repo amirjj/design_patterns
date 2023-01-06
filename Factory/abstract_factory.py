5723dd9308efcc15fa6ca1919879edb33a591d85
amirjj <amir.jamshidijam@gmail.com>
2023-01-06 13:00:42 +0330

    factory design patterns added

--- Factory/abstract_factory.py
+++ Factory/abstract_factory.py
@@ -1,2 +1,94 @@
 # combination of factories
 # any time you combine multiple Factories, you are using abstract Factory
+from abc import ABC, abstractmethod
+
+
+class ProductBase(ABC):
+    @abstractmethod
+    def detail(self):
+        pass
+
+    @abstractmethod
+    def price(self):
+        pass
+
+
+class RugPrice:
+    def __init__(self, rug):
+        self.price = rug.price_
+
+    def show(self):
+        print(f'Rug Price: {self.price}')
+
+
+class RugDetail:
+    def __init__(self, rug):
+        self.name = rug.name_
+
+    def show(self):
+        print(f'Rug Name: {self.name}.')
+
+
+class Rug(ProductBase):
+    def __init__(self, name, price):
+        self.name_ = name
+        self.price_ = price
+
+    @property
+    def detail(self):
+        return RugDetail(self)
+
+    @property
+    def price(self):
+        return RugPrice(self)
+
+
+class GiftCardDetail:
+    def __init__(self, gift_card):
+        self.company = gift_card.company
+
+    def show(self):
+        print(f'GiftCard company: {self.company}.')
+
+
+class GiftCardPrice:
+    def __init__(self, gift_card):
+        self.min_price = gift_card.min_price
+        self.max_price = gift_card.max_price
+
+    def show(self):
+        print(
+            f'GiftCard Min Price: {self.min_price}, Max Price: {self.max_price}'
+        )
+
+
+class GiftCard(ProductBase):
+    def __init__(self, company, min_price, max_price):
+        self.company = company
+        self.min_price = min_price
+        self.max_price = max_price
+
+    @property
+    def detail(self):
+        return GiftCardDetail(self)
+
+    @property
+    def price(self):
+        return GiftCardPrice(self)
+
+
+if __name__ == '__main__':
+    """
+    Showing products price and detail without knowing what is the product. 
+    complexities been hidden.
+    """
+    p1 = Rug('Kashan', 3000000)
+    p2 = Rug('Yazd', 2000000)
+    g1 = GiftCard('Google', 100, 500)
+    g2 = GiftCard('Amazon', 200, 600)
+
+    products = [p1, p2, g1, g2]
+    for p in products:
+        p.price.show()
+        p.detail.show()
+
--- Factory/factory.py
+++ Factory/factory.py
@@ -1,1 +1,64 @@
 # hide complexity, thats factory
+import random
+from abc import ABC, abstractmethod
+
+
+class BasePlayer(ABC):
+    choices = ['r', 'p', 's']
+
+    @abstractmethod
+    def move(self):
+        pass
+
+
+class HumanPlayer(BasePlayer):
+    def move(self):
+        user_input = input("select your move (r, s, p):")
+        if user_input not in self.choices:
+            print("wrong input...")
+            return self.move()
+        print(f'Human moved {user_input}')
+        return user_input
+
+
+class ComputerPlayer(BasePlayer):
+    def move(self):
+        m = random.choice(self.choices)
+        print(f'Computer Moved {m}')
+        return m
+
+
+class Game:
+    @classmethod
+    def select_play_mode(cls):
+        mode = input('select play mode: 1->Human to Human, '
+                     '2->Human to Computer, 3->Computer to Computer:\n')
+        if mode not in ['1', '2', '3']:
+            return cls.select_play_mode()
+        return mode
+
+    @classmethod
+    def run(cls):
+        mode = cls.select_play_mode()
+        if mode == '1':
+            p1 = HumanPlayer()
+            p2 = HumanPlayer()
+        elif mode == '2':
+            p1 = HumanPlayer()
+            p2 = ComputerPlayer()
+        elif mode == '3':
+            p1 = ComputerPlayer()
+            p2 = ComputerPlayer()
+        return p1, p2
+
+
+if __name__ == "__main__":
+    """
+    This part of code you just run move() method without knowing that the 
+    player is Human or Computer. Hiding complexity like this called Factory.
+    different types of Players but all have move method.
+    """
+    player1, player2 = Game.run()
+    players = [player1, player2]
+    for p in players:
+        p.move()
